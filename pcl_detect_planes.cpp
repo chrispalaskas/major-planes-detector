8812d3bbbee98deeecd7030141643fef7840370c
chrispalaskas <chris.palaskas@gmail.com>
2019-11-20 17:23:16 -0800

    Create an argument parser helper function.

--- pcl_detect_planes.cpp
+++ pcl_detect_planes.cpp
@@ -16,49 +16,49 @@
 #include <chrono>
 #include "helper.h"
 
-int main (int argc, char** argv)
+void argparser(int argc, char** argv, Helper& helper, int& totalPlanes, std::string& inputPath, double& distanceThres, bool& visualizePC)
 {
-	auto start = std::chrono::system_clock::now(); //! start: Holds the start of the program's runtime.
-	Helper helper; //! helper: Helper object, to access the class' methods.
-	std::string logfile = "log.txt"; //! logfile: The output filename of all the logging.
-	helper.setOutLog(logfile); //! Initialize the logging file.
-	std::stringstream ss; //! ss: Stringstream that will carry the message to be logged.
-
+	std::stringstream ss;
+	std::string enabledVisualization = "Visualization of Point Cloud enabled.";
+	std::string disabledVisualization = "Visualization of Point Cloud disabled.";
+	std::string optionalThirdArg = "The third argument is optional, but if set, it has to be ";
+	std::string optionalFourthArg = "The fourth argument is optional, but if set, it has to be ";
+	std::string doubleForThres = "a double for the point distance threshold, with default = 1.0.";
+	std::string boolForVisual = "a boolean true / false for visualization, with default false.";
 	if (argc < 3)
 	{
 		ss << "Please use input file name and number of planes as arguments." << std::endl
-		   << "Optional 1: distance threshold for plane detection. Default: 1.0" << std::endl
-		   << "Optional 2: enable visualization. Default: false" << std::endl
-		   << "E.g. <pcl_detect_planes.exe point_cloud.txt 3 0.1 true>";
+			<< "Optional 1: distance threshold for plane detection. Default: 1.0" << std::endl
+			<< "Optional 2: enable visualization. Default: false" << std::endl
+			<< "E.g. <pcl_detect_planes.exe point_cloud.txt 3 0.1 true>";
 		helper.writeLog(ss);
 		exit(-1);
 	}
 
-	std::string inputPath = argv[1]; //! inputPath: The path to the point cloud input file.
+	inputPath = argv[1]; //! inputPath: The path to the point cloud input file.
 	if (!helper.is_integer(argv[2])) //! Verifying that the second argument is an integer.
 	{
 		ss << "The second argument has to be an integer.";
 		helper.writeLog(ss);
 		exit(-1);
 	}
-	int totalPlanes = atoi(argv[2]); //! totalPlanes: The number of planes that should be detected.
-	double distanceThres = 1.0;
-	bool visualizePC = false;	
+	totalPlanes = atoi(argv[2]); //! totalPlanes: The number of planes that should be detected.
+
 	if (argc == 4)
 	{
 		if (std::string(argv[3]) == "true" || std::string(argv[3]) == "false")
 		{
 			visualizePC = std::string(argv[3]) == "true";
 			if (visualizePC)
-				ss << "Visualization of Point Cloud enabled.";
+				ss << enabledVisualization;
 			else
-				ss << "Visualization of Point Cloud disabled.";
+				ss << disabledVisualization;
 			helper.writeLog(ss);
 		}
 		else if (atof(argv[3]) == 0.0)
 		{
-			ss << "The third argument is optional, but if set has to be a double for the threshold with default is 1.0." << std::endl
-     		   << "OR a boolean true/false for visualization, with default false.";
+			ss << optionalThirdArg << doubleForThres << std::endl
+				<< "OR " << boolForVisual;
 			helper.writeLog(ss);
 			exit(-1);
 		}
@@ -69,31 +69,44 @@ int main (int argc, char** argv)
 	{
 		if (atof(argv[3]) == 0.0)
 		{
-			ss << "The third argument is optional, but if set has to be a double for the threshold with default is 1.0.";
+			ss << optionalThirdArg << doubleForThres;
 			helper.writeLog(ss);
 			exit(-1);
 		}
 		else
 			distanceThres = atof(argv[3]);
-		
+
 		if (std::string(argv[4]) == "true" || std::string(argv[4]) == "false")
 		{
 			visualizePC = std::string(argv[4]) == "true";
 			if (visualizePC)
-				ss << "Visualization of Point Cloud enabled.";
+				ss << enabledVisualization;
 			else
-				ss << "Visualization of Point Cloud disabled.";
+				ss << disabledVisualization;
 			helper.writeLog(ss);
 		}
 		else
 		{
-			ss << "The fourth argument is optional, but if set has to be a boolean true/false for visualization, with default false.";
+			ss << optionalFourthArg << boolForVisual;
 			helper.writeLog(ss);
 			exit(-1);
 		}
-
 	}
+}
 
+int main (int argc, char** argv)
+{
+	auto start = std::chrono::system_clock::now(); //! start: Holds the start of the program's runtime.
+	Helper helper; //! helper: Helper object, to access the class' methods.
+	std::string logfile = "log.txt"; //! logfile: The output filename of all the logging.
+	helper.setOutLog(logfile); //! Initialize the logging file.
+
+	int totalPlanes=0;
+	std::string inputPath="";
+	double distanceThres = 1.0;
+	bool visualizePC = false;
+	argparser(argc, argv, helper, totalPlanes, inputPath, distanceThres, visualizePC);
+	
 	
 	std::string outputPlanes = "Detected_Planes.txt"; //! outputPlanes: The output filename with the detected planes, centroid and normal.
 	std::string outputPointCloudWithPlaneIDs = "Point_Cloud_with_Plane_id.txt"; //! outputPointCloudWithPlaneIDs: The output filename with the cloud points and plane_id.
@@ -110,6 +123,7 @@ int main (int argc, char** argv)
 	outfileCloudWPlanes.close();
 	auto end = std::chrono::system_clock::now(); //! end: Holds the end of the program's runtime.
 	std::chrono::duration<double> elapsed_seconds = end - start; //! elapsed_seconds: Holds the duration of the runtime in seconds.
+	std::stringstream ss; //! ss: Stringstream that will carry the message to be logged.
 	ss << "Elapsed time: " << elapsed_seconds.count() << "s";
 	helper.writeLog(ss);
 	return (0);
